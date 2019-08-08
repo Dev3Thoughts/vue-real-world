19be51added32f45ad1a81faeaaaaab863ad8c5e
Majad in <majad.inayat@sogeti.com>
2019-08-08 15:34:52 +0200

    Vuex mapState simple

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <h1>Create an Event, {{ userName }}</h1>
-    <p>This event was created by {{ userName }}</p>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,9 +13,8 @@ import { mapState } from 'vuex'
 
 export default {
   computed: mapState({
-    userName: state => state.user.name,
-    userID: state => state.user.id,
-    categories: state => state.categories
+    user: 'user',
+    categories: 'categories'
   })
 }
 </script>
